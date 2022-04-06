cf92e4bb1304e397352467ab6b986aabd4811872
ImDeukGyun <100827522+ImDeukGyun@users.noreply.github.com>
2022-04-06 16:39:43 +0900

    Add files via upload

--- hw41.php
+++ hw41.php
@@ -1,11 +1,37 @@
+<!DOCTYPE html>
 <html>
 <body>
-<meta charset="utf-8">
 
-<form action="hw41.php" method="get">
-정수 : <input type="text" name="name"><br>
-<input type="submit">
-</form>
+
+<?php
+
+$n = $_POST["name"];
+$sum = 0;
+$prod = 0;
+
+for ($x = 1; $x <= $n-1; $x+=1) {
+  echo "$x+";
+  $prod += $x;
+  $sum = $prod;
+}
+$sum = $prod += $n;
+print "$n=$sum<br>"
+?>
+<?php
+
+$n = $_POST["name"];
+$sum = 0;
+$prod = 1;
+
+for ($x = 1; $x <= $n-1; $x+=1) {
+  echo "$x*";
+  $prod *= $x;
+  $sum = $prod;
+}
+$sum = $prod *= $n;
+
+print "$n=$sum"
+?>  
 
 </body>
 </html>
